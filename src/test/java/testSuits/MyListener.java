550b699c58c6218b382e28e383a26607bd12cdd5
vstarchevskyi <v.starchevskyi@zakaz.global>
2023-10-15 14:07:10 +0300

    add MyListener

---
+++ src/test/java/testSuits/MyListener.java
@@ -0,0 +1,15 @@
+package testSuits;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class MyListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+        System.out.println(result.getName() + " test started");
+    }
+}
