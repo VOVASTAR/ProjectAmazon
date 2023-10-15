65c7507e090c7af7b9a86e59df817bbc3c6c126e
vstarchevskyi <v.starchevskyi@zakaz.global>
2023-10-15 23:53:53 +0300

    before method add webdriverManager

--- src/test/java/negativeTest/IncorrectEmail.java
+++ src/test/java/negativeTest/IncorrectEmail.java
@@ -3,6 +3,7 @@ package negativeTest;
 import amazon.page.object.MainPageLogic;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
@@ -17,7 +18,7 @@ public class IncorrectEmail {
 
     @BeforeMethod
     public void before() {
-
+        WebDriverManager.chromedriver().setup();
 //        Configuration.browser = "chrome";
         ChromeOptions chromeOptions = getChromeOptions();
         Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
--- src/test/java/positiveTest/UserSignIn.java
+++ src/test/java/positiveTest/UserSignIn.java
@@ -4,6 +4,7 @@ import amazon.page.object.MainPageLogic;
 import amazon.page.object.YourStorePageLogic;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
@@ -18,7 +19,7 @@ public class UserSignIn {
 
     @BeforeTest
     public void before() {
-
+        WebDriverManager.chromedriver().setup();
 //        Configuration.browser = "chrome";
         ChromeOptions chromeOptions = getChromeOptions();
         Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
